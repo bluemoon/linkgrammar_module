09611587804282b0590d6425ca4f5f971fed5178
Alex Toney <toneyalex@gmail.com>
2010-06-28 03:53:45 -0700

    adding the files

---
+++ README.rst
@@ -0,0 +1,15 @@
+Overview
+========
+An interface to link grammar to build and extend the idea
+
+Pre-requisites
+==============
+link grammar
+python :)
+
+Install
+=======
+To install:
+1. python setup.py build
+2. sudo python setup.py install
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup, Extension
+link_grammar = Extension("linkGrammar",
+                         libraries = ['link-grammar'],
+                         library_dirs = ['/usr/local/lib'],
+                         include_dirs = ['/usr/local/include/link-grammar'],
+                         sources=["linkgrammar.c"])
+
+
+setup(name = "linkGrammar",
+      version = "1.0",
+      author = 'Alex Toney',
+      ext_modules = [link_grammar]
+      )
